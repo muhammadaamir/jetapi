f3f102b0cb8162eb13fec77520a0ac618081ad7d
sidraaa <sidraferoze@yahoo.com>
2016-11-01 17:13:03 +0500

    Rakuten library method

---
+++ application/controllers/Rakuten.php
@@ -0,0 +1,48 @@
+<?php
+
+class Rakuten extends CI_Controller{
+    function __Construct() {
+        parent::__Construct();
+        //$this->load->library('Rakuten'); // load libbrary
+        $this->load->model('ProductModel');
+    }
+    
+    public function index(){
+        echo "in Rakuten controller";
+    }
+    
+    public function write(){
+        
+        //....**** direct method ..*****
+//        $this->load->dbutil();
+//        $object=$this->ProductModel->mp_feed();
+//        $csv = $this->dbutil->csv_from_result($object);
+//        write_file(FCPATH."/assets/csv_files/mp_feed.csv", $csv);
+        
+        
+        $this->load->library('Rakuten');      
+        $result=$this->rakuten->Func();
+        if($result)
+            echo "success";
+    }
+    
+    
+    
+    
+    public function conn() {
+//        $this->load->library('ftp');
+//        $config['hostname']='trade.marketplace.buy.com';
+//        if($this->ftp->connect($config)){
+//            echo "connected";
+//        }
+//        else echo "could not connect.";
+        
+        
+        $conn_id= ftp_connect("trade.marketplace.buy.com");
+        if($conn_id){
+            echo "connected";
+        }
+        else echo "could not connect";
+    }
+}
+
