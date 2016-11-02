5f32a09c41460823874b6873b79865c13eb94163
sidraaa <sidraferoze@yahoo.com>
2016-11-02 12:42:51 +0500

    Products and quantity functions added

--- application/controllers/Rakuten.php
+++ application/controllers/Rakuten.php
@@ -3,32 +3,36 @@
 class Rakuten extends CI_Controller{
     function __Construct() {
         parent::__Construct();
-        //$this->load->library('Rakuten'); // load libbrary
         $this->load->model('ProductModel');
+        $this->load->library('RakutenApi'); 
     }
     
     public function index(){
         echo "in Rakuten controller";
     }
     
-    public function write(){
-        
-        //....**** direct method ..*****
-//        $this->load->dbutil();
-//        $object=$this->ProductModel->mp_feed();
-//        $csv = $this->dbutil->csv_from_result($object);
-//        write_file(FCPATH."/assets/csv_files/mp_feed.csv", $csv);
-        
-        
-        $this->load->library('Rakuten');      
-        $result=$this->rakuten->Func();
+    public function products(){  
+        $path = FCPATH."/assets/csv_files/sku_feed.csv";
+        $RakutenApi = new RakutenApi();   
+        $result=$RakutenApi->products($path);
         if($result)
-            echo "success";
+            echo "Products csv written to the file ".$path;
     }
     
+    public function quantity(){
+        $path = FCPATH."/assets/csv_files/mp_feed.csv";
+        $RakutenApi = new RakutenApi();   
+        $result=$RakutenApi->quantity($path);
+        if($result)
+            echo "Quantity csv written to the file ".$path;
+    }
+
     
-    
-    
+
+
+
+
+
     public function conn() {
 //        $this->load->library('ftp');
 //        $config['hostname']='trade.marketplace.buy.com';
--- application/libraries/RakutenApi.php
+++ application/libraries/RakutenApi.php
@@ -6,36 +6,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    class Rakuten{
+    class RakutenApi{
         function __Construct() {
             $this->CI =& get_instance();
             $this->CI->load->model('ProductModel');
             $this->CI->load->helper('file');
         }
-        static $listing_id = 'listing-id';
-        static $seller_sku = 'seller-sku';
-        static $item_name = 'item-name';
-        static $item_desc = 'item-description';
-        static $product_id = 'product-id';
-        static $product_id_type = 'product-id-type';
-        static $item_condition = 'item-condition';
-        static $expedited_shipping = 'expedited-shipping';
         
-        
-        public function Func(){
-            return 'function';
-        }
-        
-        public function products(){
-            $wb= fopen(FCPATH."/assets/csv_files/sku_feed.csv", 'wb');
-            $a= fopen(FCPATH."/assets/csv_files/sku_feed.csv", 'a');
+        public function products($path){
+            $listing_id = 'listing-id';
+            $seller_sku = 'seller-sku';
+            $item_name = 'item-name';
+            $item_desc = 'item-description';
+            $product_id = 'product-id';
+            
+            $wb= fopen($path, 'wb');
+            $a= fopen($path, 'a');
             $headings=array('gtin','mfg-name','asin','seller-sku','title','description',
                 'main-image','listing-price','mfg-part-number','category-id','seller-id','weight');
             $static_fields=array('0349','ABCD','1kg');
 
             fputcsv($wb, $headings);
 
-            $array=$this->ProductModel->getProduct();
+            $array=$this->CI->ProductModel->getProduct();
             foreach ($array as $row){
                 $new_array = array(
                     $row->$listing_id,$row->manufacturer,$row->asin1,$row->$seller_sku,
@@ -49,21 +42,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         }
         
         
-        public function quantity(){
-            $wb= fopen(FCPATH."/assets/csv_files/mp_feed.csv", 'wb');
-            $a= fopen(FCPATH."/assets/csv_files/mp_feed.csv", 'a');
+        public function quantity($path){
+            $listing_id = 'listing-id';
+            $seller_sku = 'seller-sku';
+            $item_desc = 'item-description';
+            $product_id = 'product-id';
+            $product_id_type = 'product-id-type';
+            $item_condition = 'item-condition';
+            $expedited_shipping = 'expedited-shipping';
+            
+            $wb= fopen($path, 'wb');
+            $a= fopen($path, 'a');
             $headings = array(
                 'ListingId','ProductId','ProductIdType','ItemCondition','Price','Quantity',
                 'OfferExpeditedShipping','Description','ReferenceId');
             fputcsv($wb, $headings);
 
-            $array=$this->ProductModel->getProduct();
+            $array=$this->CI->ProductModel->getProduct();
             foreach ($array as $row){
                 $new_array = array(
                     $row->$listing_id,$row->$product_id,$row->$product_id_type,$row->$item_condition,
                    $row->price,$row->quantity,$row->$expedited_shipping,$row->$item_desc,$row->$seller_sku);
-                fputcsv($a, $new_array);
+                if(!fputcsv($a, $new_array)){
+                    return FALSE;
+                }
             }
+            return TRUE;
         }
 
         
